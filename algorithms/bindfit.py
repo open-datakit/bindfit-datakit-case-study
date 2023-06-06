b81816780d3e143e1b294ff6343f9552a08f55af
echus <varvara@echus.co>
2023-06-06 21:05:34 +1000

    Add options input, use all inputs in algorithm

--- algorithms/bindfit.py
+++ algorithms/bindfit.py
@@ -1,35 +1,42 @@
 def main(
-    # data,
-    # params,
-    # options,
+    data,
+    params,
+    options,
     **kwargs,
 ):
     import numpy as np
+    import pandas as pd
 
     import bindfit
 
-    # Parameters and options
-    params = {
-        "k": {
-            "init": 100.0,
-            "bounds": {
-                "min": 0.0,
-                "max": None,
-            },
-        },
-    }
 
-    fitter_name = "nmr1to1"
-    method = "Nelder-Mead"
-    normalise = True
-    dilute = False
-    flavour = "none"
+    # Convert params to Bindfit format
+    # TODO: Modify library to accept Frictionless params format
+    bindfit_params = {}
 
-    # Load raw data
-    data = np.genfromtxt("input.csv", delimiter=",", skip_header=1)
+    for key, param in params["data"].items():
+        bindfit_params.update({
+            key: {
+                "init": param["value"],
+                "bounds": {
+                    "min": param["lowerBound"],
+                    "max": param["upperBound"],
+                },
+            }
+        })
+
+    # Bindfit options
+    model = options["data"]["model"]
+    method = options["data"]["method"]
+    normalise = options["data"]["normalise"]
+    dilute = options["data"]["dilute"]
+    flavour = options["data"]["flavour"]
+
+    # Load data
+    df = pd.DataFrame.from_dict(data["data"])
     # Bindfit expects each variable as rows
-    data_x = np.transpose(data[:, :2])
-    data_y = np.transpose(data[:, 2:])
+    data_x = np.transpose(df.iloc[:, :2].to_numpy())
+    data_y = np.transpose(df.iloc[:, 2:].to_numpy())
 
     # Apply dilution correction
     # TODO: Think about where this should go - fitter or function?
@@ -38,19 +45,19 @@ def main(
         data_y = bindfit.helpers.dilute(data_x[0], data_y)
 
     function = bindfit.functions.construct(
-        fitter_name,
+        model,
         normalise=normalise,
         flavour=flavour,
     )
 
     fitter = bindfit.fitter.Fitter(
-        data_x, data_y, function, normalise=normalise, params=params
+        data_x, data_y, function, normalise=normalise, params=bindfit_params
     )
 
-    fitter.run_scipy(params, method=method)
+    fitter.run_scipy(bindfit_params, method=method)
 
     summary = {
-        "fitter": fitter_name,
+        "fitter": model,
         "fit": {
             "y": fitter.fit,
             "coeffs_raw": fitter.coeffs_raw,

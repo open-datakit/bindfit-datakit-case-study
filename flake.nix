f8344740babec415f52a3a7c165d01dd00ffabb5
echus <varvara@echus.co>
2023-06-06 13:49:17 +1000

    Add flake and direnv config

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "";
+
+  # Use the unstable nixpkgs to use the latest set of node packages
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          # Set the major version of Node.js
+          pkgs.python310
+          pkgs.python310Packages.numpy
+          pkgs.python310Packages.scipy
+          pkgs.python310Packages.pandas
+        ];
+      };
+    });
+}

db58b355e37b35d2d538cb9d8a6a6e40e74b41d6
echus <varvara@echus.co>
2024-05-07 15:36:15 +1000

    Add fit curve and residuals outputs, make data schema external

--- algorithms/bindfit.py
+++ algorithms/bindfit.py
@@ -6,25 +6,31 @@ def main(
     subInitValues,
     dilutionCorrection,
     flavour,
+    fitCurve,
+    fitResiduals,
 ):
     """Construct and run a Bindfit fitter given a dataset and parameters
 
-    Arguments
-    ----------
-    data: `list`
+    Parameter space
+    ---------------
+    data: `tabular-data-resource`
         Tabular input data in array of named row objects format
     fitMethod: `str`
         The optimisation algorithm to use for fitting
     fitModel: `str`
         The model to fit to the data
-    fitModelParams: `list`
-        Table of input parameters containing initial values and bounds
+    fitModelParams: `tabular-data-resource`
+        Table of parameters containing initial guesses and output values
     subInitValues: `bool`
         If true, subtract the first column from all data before fitting
     dilutionCorrection: `bool`
         If true, apply dilution correction to data before fitting
     flavour: `str` or None
         If not None, one of "add", "stat" or "noncoop"
+    fitCurve: `tabular-data-resource`
+        Output fit curve
+    fitResiduals: `tabular-data-resource`
+        Output fit residuals
 
     Returns
     -------
@@ -37,14 +43,6 @@ def main(
     # Error checks
     if not data:
         raise ValueError("No data passed to algorithm")
-
-    # Munge data into Bindfit format
-    # Bindfit expects each variable as row
-    # np.asarray(list()) required to convert index list of tuples to normal
-    # numpy matrix
-    # TODO: Move this to a helper function in opendatafit.TabularDataResource
-    data_x = np.transpose(np.asarray(list(data.data.index.to_numpy())))
-    data_y = np.transpose(data.data.to_numpy())
 
     # Convert param initial values and bounds to Bindfit format
     input_params = {}
@@ -62,12 +60,6 @@ def main(
             }
         )
 
-    # Apply dilution correction
-    # TODO: Think about where this should go - fitter or function?
-    # Or just apply it before the fit?
-    if dilutionCorrection:
-        data_y = bindfit.helpers.dilute(data_x[0], data_y)
-
     # Construct and run Bindfit fitter
     function = bindfit.functions.construct(
         fitModel,
@@ -76,7 +68,11 @@ def main(
     )
 
     fitter = bindfit.fitter.Fitter(
-        data_x, data_y, function, normalise=subInitValues, params=input_params
+        data=data.data,
+        function=function,
+        params=input_params,
+        normalise=subInitValues,
+        dilution_correction=dilutionCorrection,
     )
 
     fitter.run_scipy(input_params, method=fitMethod)
@@ -85,9 +81,15 @@ def main(
 
     # Write optimised parameter values
     for key, result in fitter.params.items():
-        fitModelParams.data.loc["k", "value"] = result["value"]
-        fitModelParams.data.loc["k", "stderr"] = result["stderr"]
+        fitModelParams.data.loc[key, "value"] = result["value"]
+        fitModelParams.data.loc[key, "stderr"] = result["stderr"]
+
+    # Write output fit and residuals data
+    fitCurve.data = fitter.fit_curve()
+    fitResiduals.data = fitter.fit_residuals()
 
     return {
-        "fitModelParams": fitModelParams
+        "fitModelParams": fitModelParams,
+        "fitCurve": fitCurve,
+        "fitResiduals": fitResiduals,
     }

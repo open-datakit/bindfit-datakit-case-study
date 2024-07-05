f34a1237e5dea8eedb47ab59d05545ea30c0dcf5
echus <varvara@echus.co>
2024-07-05 16:15:11 +1000

    Modify datapackage container API - don't pass algorithm name along with argument space

--- test_datapackage.sh
+++ test_datapackage.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 # Build bindfit container first with containers/build.sh script
-docker run -it -v ${PWD}:/usr/src/app/datapackage -e ALGORITHM=bindfit -e CONTAINER=opendatafit/bindfit -e ARGUMENTS=bindfit.default opendatafit/bindfit
+docker run -it -v ${PWD}:/usr/src/app/datapackage -e ALGORITHM=bindfit -e CONTAINER=opendatafit/bindfit -e ARGUMENTS=default opendatafit/bindfit

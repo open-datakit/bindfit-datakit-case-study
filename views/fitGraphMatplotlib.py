f32125459625a083c5bc92f89c4154987529fb40
echus <varvara@echus.co>
2024-07-12 18:42:54 +1000

    Add matplotlib graphing function

--- views/fitGraphMatplotlib.py
+++ views/fitGraphMatplotlib.py
@@ -2,35 +2,38 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
-from scipy.optimize import curve_fit
 
 
 def main(
     data: pd.DataFrame, fitCurve: pd.DataFrame, fitResiduals: pd.DataFrame
 ) -> Figure:
-    # Data
-    x = np.arange(1, 10, 0.2)
-    ynoise = x * np.random.rand(len(x))
-    ydata = x**2 + ynoise
-
-    Fofx = lambda x, a, b, c: a * x**2 + b * x + c  # noqa: E731
-    p, cov = curve_fit(Fofx, x, ydata)
+    # Get data
+    index = np.array(list(data.data.index.to_numpy()))
+    x = index[:, 1] / index[:, 0]  # guest/host
+    y = data.data.to_numpy()
+    y_fit = fitCurve.data.to_numpy()
+    y_residuals = fitResiduals.data.to_numpy()
 
     # Upper plot
-    fig1 = plt.figure(1)
-    frame1 = fig1.add_axes((0.1, 0.3, 0.8, 0.6))
+    fig = plt.figure(1)
+    upper_frame = fig.add_axes((0.1, 0.3, 0.8, 0.6))
 
-    plt.plot(x, ydata, ".b")
-    plt.plot(x, Fofx(x, *p), "-r")
+    plt.plot(x, y, ".b")
+    plt.plot(x, y_fit, "-r")
 
-    frame1.set_xticklabels([])
+    plt.xlabel("[G]/[H]")
+    plt.ylabel(r"$\rho$")
+
+    upper_frame.set_xticklabels([])
     plt.grid()
 
-    # Residual plot
-    difference = Fofx(x, *p) - ydata
-    fig1.add_axes((0.1, 0.1, 0.8, 0.2))
-    plt.plot(x, difference, "or")
+    # Residuals plot
+    fig.add_axes((0.1, 0.1, 0.8, 0.2))
+    plt.plot(x, y_residuals, ".r")
     plt.grid()
+
+    plt.xlabel("[G]/[H]")
+    plt.ylabel(r"$\rho$")
 
     # Return current figure
     return plt.gcf()

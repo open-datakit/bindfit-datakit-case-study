a9ce052af081f56179ceaf7e8f3c2027068a3830
Varvara Efremova <varvara@echus.co>
2024-09-05 15:39:09 +1000

    Rename arguments -> configurations

--- run_datapackage.sh
+++ run_datapackage.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 # Build bindfit container first with containers/build.sh script
-docker run -it -v ${PWD}:/usr/src/app/datapackage -e ALGORITHM=bindfit -e ARGUMENT_SPACE=default ods/bindfit
+docker run -it -v ${PWD}:/usr/src/app/datapackage -e CONFIGURATION=bindfit.default ods/bindfit
